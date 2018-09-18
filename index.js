915ead05d3f1b515f27b2d3fa4111ca6b596e889
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-09-18 00:24:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
   else{
     for(i = 0; i < cart.length; i++){
       if(getCart()[i].itemName === item){
-        return cart.splice(i,1);
+        return cart.splice(1, i);
       }
     }
   }
