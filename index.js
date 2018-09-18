e300f34130d1ff89bd0238e97741030e937b785c
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-09-18 00:08:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,6 +65,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(cardNumber === null){
-    return `Sorry, we don't have a credit card on file for you.`:
+    return `Sorry, we don't have a credit card on file for you.`;
   }
 }
+
+
+
+
